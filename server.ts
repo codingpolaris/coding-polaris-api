2ae5d8f95420cf786b8df2c2722616da8f920fa4
CaveiraRussa <luiz.m.santiago@hotmail.com>
2021-11-03 17:53:33 -0300

    WIP: Developing database connection

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
 import express, {Request, Response} from 'express';
+import { createConnection, ConnectionOptions, } from 'typeorm';
 import {UserController } from './src/controller/userController';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 class Server {
+
     private userController: UserController;
 
     private app: express.Application;
@@ -17,11 +21,27 @@ class Server {
         this.app.set('port', process.env.PORT || 8080);
     }
 
-    public routes(){
-        this.app.use('/api/user/', this.userController.router);
+    options:ConnectionOptions = {
+        type:'postgres',
+        host:process.env.HOST,
+        port:Number(process.env.PORT),
+        username:process.env.USER,
+        password:process.env.PASSWORD,
+        database:process.env.DATABASE,
+        entities:["build/database/entities/**/*.js"],
+        synchronize:true,
+        name:process.env.NAME,
+    }
+    public async routes(){
+        await createConnection(this.options);
+
+        this.userController = new UserController();
+
         this.app.get("/", (req: Request, res: Response) => {
             res.send("Hallo Welt")
         });
+
+        this.app.use('/api/user/', this.userController.router);
     }
 
     public start(){
@@ -29,6 +49,7 @@ class Server {
             console.log(`Server is listening ${this.app.get('port')} port.`);
         });
     }
+
 }
 
 const server = new Server();
