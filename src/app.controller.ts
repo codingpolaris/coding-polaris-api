61fbd88a29cd6f6638a6ebf08348e4da1d8420e3
CaveiraRussa <luiz.m.santiago@hotmail.com>
2021-11-13 15:48:44 -0300

    feat: profile returning all user info

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,14 @@ import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
@@ -17,6 +21,6 @@ export class AppController {
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
-    return req.user;
+    return this.usersService.findUsername(req.user.username);
   }
 }
