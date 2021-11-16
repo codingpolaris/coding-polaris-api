b217173968a8262aca9b35d7a06f55d4880458f5
CaveiraRussa <luiz.m.santiago@hotmail.com>
2021-11-16 17:06:50 -0300

    bugfix: fixing env problem

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -1,8 +1,11 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 class Configs {
-  public host = process.env.HOST;
-  public port = process.env.PORT;
-  public user = process.env.USER;
-  public password = process.env.PASSWORD;
+  public host = process.env.EMAIL_HOST;
+  public port = process.env.EMAIL_PORT;
+  public user = process.env.EMAIL_USER;
+  public password = process.env.EMAIL_PASSWORD;
 }
 
 export default new Configs();
