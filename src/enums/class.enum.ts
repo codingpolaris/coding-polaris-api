6b2fad0e2dfffffb9ac1c6dd8db972a5e43705d5
CaveiraRussa <luiz.m.santiago@hotmail.com>
2021-11-20 18:55:06 -0300

    feature: inserting new classes

--- src/enums/class.enum.ts
+++ src/enums/class.enum.ts
@@ -1,7 +1,7 @@
 export enum Class {
   'Garoto de TI' = 1,
-  'Iniciante' = 2,
-  'HTML' = 3,
+  'Mago do versionamento' = 2,
+  'Hiper Marcador' = 3,
   'Cavaleiro da Cascata' = 4,
-  'JAVASCRIPT' = 5,
+  'Black Kamen Jyder' = 5,
 }
