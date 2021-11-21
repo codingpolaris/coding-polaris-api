304e3734ddcebecd7a216475c0427965585811f9
CaveiraRussa <luiz.m.santiago@hotmail.com>
2021-11-20 21:46:34 -0300

    bugfix: fixing bug for frontend

--- src/characters-themes/services/response/characters-themes.response.ts
+++ src/characters-themes/services/response/characters-themes.response.ts
@@ -4,11 +4,13 @@ import { Theme } from 'src/themes/entities/theme.entity';
 export class ResponseCharactersThemes extends Theme {
   constructor(characterTheme: CharactersTheme) {
     super();
-    this.id = characterTheme.theme.id;
+    this.id = characterTheme.id;
+    this.themeId = characterTheme.theme.id;
     this.name = characterTheme.theme.name;
     this.level = characterTheme.theme.level;
     this.acess = characterTheme.theme.acess;
     this.isCompleted = characterTheme.isCompleted;
   }
+  themeId: number;
   isCompleted: boolean;
 }
